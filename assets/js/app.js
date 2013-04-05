278298fb41c9ee95e34c0809a74b9d35712bc31c
Flo-Schield-Bobby <florent.schildknecht@gmail.com>
2013-04-05 17:13:07 +0200

    Add a uggly bird animation

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,54 +1,158 @@
 jQuery(document).on('ready', function () {
 	if (zig) {
-		var users = [],
-			maxUsersAllowed = 1,
-			posx = 0,
+		var posx = 0,
 			posy = 0,
 			posz = 0,
 			engager = zig.EngageUsersWithSkeleton(1),
 			$doc = jQuery(document),
 			$kinectX = jQuery('#kinect-x'),
 			$kinectY = jQuery('#kinect-y'),
-			$kinectZ = jQuery('#kinect-z');
-			
-		engager.addEventListener('userengaged', function(user) {
-			if (users.length < maxUsersAllowed) {
-				users.push(user);
+			$kinectZ = jQuery('#kinect-z'),
+			onUserMove = function(user) {
 				posx = user.skeleton[zig.Joint.Head].position[0];
 				posy = user.skeleton[zig.Joint.Head].position[1];
 				posz = user.skeleton[zig.Joint.Head].position[2];
-				$doc.trigger('headstart', {
+				$kinectX.text(posx);
+				$kinectY.text(posy);
+				$kinectZ.text(posz);
+				$doc.trigger('headmove', {
 					'x': posx,
 					'y': posy,
 					'z': posz
 				});
+			};
+			
+		engager.addEventListener('userengaged', function(user) {
+			console.log('user engaged', user.id);
+			user.addEventListener('userupdate', onUserMove);
 
-				user.addEventListener('userupdate', function(user) {
-					posx = user.skeleton[zig.Joint.Head].position[0];
-					posy = user.skeleton[zig.Joint.Head].position[1];
-					posz = user.skeleton[zig.Joint.Head].position[2];
-					$kinectX.text(posx);
-					$kinectY.text(posy);
-					$kinectZ.text(posz);
-					$doc.trigger('headmove', {
-						'x': posx,
-						'y': posy,
-						'z': posz
-					});
-				});
-			}
+			posx = user.skeleton[zig.Joint.Head].position[0];
+			posy = user.skeleton[zig.Joint.Head].position[1];
+			posz = user.skeleton[zig.Joint.Head].position[2];
+			$doc.trigger('headstart', {
+				'x': posx,
+				'y': posy,
+				'z': posz
+			});
 		});
 
 		engager.addEventListener('userdisengaged', function(user) {
-			var userIndex = $.inArray(user, users);
-			if (userIndex > -1) {
-				users.splice(userIndex, 1);
-				$doc.trigger('headstop');
-			}
+			console.log('user disengaged', user.id);
+			console.log('removeEventListener', user.id);
+			user.removeEventListener('userupdate', onUserMove);
+			$doc.trigger('headstop');
 		});
 
 		zig.addListener(engager);
 	} else {
 		alert('Zigfu cannot be load :(');
 	}
+
+	var x =  -10;
+    var y = 75;
+    var speed = 2;
+    var i = j = l= 1;
+
+
+	// Bird animation
+	var initPosition = {
+			'top': 75,
+			'left': -10
+		},
+		position = {
+			'top': initPosition.top,
+			'left': initPosition.left
+		},
+		speed = {
+			'x': 2,
+			'y': 0
+		},,
+		$bird = $('#bird-1')
+		loopLimit = {
+			'x': 600,
+			'y': initPosition.top
+		},
+		i = 0,
+		sprite = {
+			'frames': [{
+				'src': 'assets/images/sprites/bird-1.png',
+				'x': 0,
+				'y': 0,
+				'width': 175,
+				'height': 180
+			},
+			{
+				'src': 'assets/images/sprites/bird-2.png',
+				'x': 0,
+				'y': 0,
+				'width': 175,
+				'height': 180
+			},
+			{
+				'src': 'assets/images/sprites/bird-3.png',
+				'x': 0,
+				'y': 0,
+				'width': 175,
+				'height': 180
+			},
+			{
+				'src': 'assets/images/sprites/bird-4.png',
+				'x': 0,
+				'y': 0,
+				'width': 175,
+				'height': 180
+			}]
+		};
+
+	(function animateBird () {
+		window.RAF(animateBird);
+		position.left += speed.x;
+		if (position.left >= loopLimit.x) {
+			position.left = initPosition.left;
+		}
+		if (i === sprite.frames.length) {
+			i = 0;
+		}
+		$bird.clearCanvas().drawImage({
+			source: sprite.frames[i].src,
+			x: position.left,
+			width: 20,
+			height: 21,
+			fromCenter: false
+		});
+		if (position.left % 20 === 0) {
+			i++;
+		}
+	})();
+	/*
+	function animate() {
+	reqAnimFrame(animate);
+
+	x += speed;
+	if(x==800)
+	x = -10;
+
+	if(l == 4) l=1;
+	draw();
+	}
+
+
+	function draw() {
+	var canvas  = document.getElementById("ex1");
+	var context = canvas.getContext("2d");
+
+	context.clearRect(0, 0, 600, 200);
+	context.beginPath();
+	context.fillStyle="#0000ff";
+
+	var bird = new Image();
+	bird.src = "/images/bird"+l+".png";
+	if(x%20 ==0){
+	l++;}
+	context.drawImage(bird, x, 20);
+	context.fillRect(0,170, 600, 10);
+	}
+
+	animate();
+	*/
 });
--- assets/js/plugins.js
+++ assets/js/plugins.js
@@ -21,13 +21,42 @@
 	}
 }());
 
-// Place any jQuery/helper plugins in here.
+// HELPERS / PLUGINS CONFIG //
+
+// requestAnimationFrame polyfill from Erik Möller - Opera developer
+// http://my.opera.com/emoller/blog/2011/12/20/requestanimationframe-for-smart-er-animating
+window.RAF = window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || window.msRequestAnimationFrame || window.oRequestAnimationFrame;
+if (!window.RAF) {
+	var lastTime = 0;
+	window.RAF = function (callback, element) {
+		var currTime = new Date().getTime(),
+			timeToCall = Math.max(0, 16 - (currTime - lastTime)),
+			id = window.setTimeout(function() {
+				callback(currTime + timeToCall);
+			}, timeToCall);
+			lastTime = currTime + timeToCall;
+		return id;
+	};
+}
+window.CRAF = window.webkitCancelRequestAnimationFrame || window.mozCancelRequestAnimationFrame || window.msCancelRequestAnimationFrame || window.oCancelRequestAnimationFrame;
+if (!window.CRAF) {
+	window.CRAF = function(id) {
+        clearTimeout(id);
+    };
+}
+
+// Hide zigfu advertising element
 $(document).ready(function () {
 	var $zigfuLink,
 		checkInterval = setInterval(function () {
 			$zigfuLink = $('div').find('a[href="http://zigfu.com/watermark"]');
 			if ($zigfuLink.length > 0) {
-				$zigfuLink.parent().remove();
+				// Zigfu REQUIRE this element to be visible... ?!
+				$zigfuLink.parent().css({
+					'width': '1px',
+					'height': '1px',
+					'overflow': 'hidden'
+				});
 				clearInterval(checkInterval);
 			}
 		}, 50);
